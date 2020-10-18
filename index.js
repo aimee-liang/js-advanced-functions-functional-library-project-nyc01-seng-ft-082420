26907da23a942f3d85308d3779af2fec5fc0a1e4
aimee-liang <aimeeeliang@gmail.com>
2020-10-17 22:13:21 -0400

    Working on lab...

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
 
     },
 
-    map: function() {
+    map: function(collection, callback){
+      if (!(collection instanceOf Array)){
+        collection = Object.values(collection)
+      }
 
+      let newArray = []
+
+      for (let i = 0; i < collection.length; i++){
+        newArray.push(callback(collection[i]))
+      }
+      return newArray
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      
     },
 
     functions: function() {
